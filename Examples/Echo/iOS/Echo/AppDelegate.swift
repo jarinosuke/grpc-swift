6a1a730424a31d786dc4b5c2f8935c6940303981
Yosuke Ishikawa <y@ishkawa.org>
2017-10-06 11:37:30 +0900

    Add iOS example project

---
+++ Examples/Echo/iOS/Echo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Echo
+//
+//  Created by Yosuke Ishikawa on 2017/10/06.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Examples/Echo/iOS/Echo/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Echo
+//
+//  Created by Yosuke Ishikawa on 2017/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
